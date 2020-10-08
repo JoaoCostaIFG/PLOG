dfac81d1a3c3f74b5e9744c76e318c58d9342eb2
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-10-08 14:59:14 +0100

    Add: solved primos exercise.

--- Class02/primos.pl
+++ Class02/primos.pl
@@ -1,1 +1,12 @@
+e_primo(2).
+e_primo(3).
 e_primo(N) :-
+  N > 3,
+  N mod 2 =\= 0,
+  \+has_factor(N, 3).
+
+has_factor(N, D) :- N mod D =:= 0.
+has_factor(N, D) :-
+  D * D < N,
+  D2 is D + 2,
+  has_factor(N, D2).
