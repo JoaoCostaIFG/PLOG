fe879e4bea2193ef6a8143e166eca4aac7008f17
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-10-15 17:40:04 +0100

    Started proj.

---
+++ Proj/PLOG_TP1_RI_T3_Emulsion_1.pl
@@ -0,0 +1,75 @@
+playIO :-
+  initial(-GameState),
+  Player is 0,
+  display_game(+GameState, +Player).
+
+% draws a given board/GameState on the console
+display_game(+GameState, +Player) :-
+  print_board(GameState).
+
+print_board([]).
+print_board([L | B]) :-
+  list_length(L, Len),
+  print_line_top(Len),
+  print_line(L), nl,
+  print_line_bot(Len),
+  print_board(B).
+
+% ┌───┐
+% │ W │
+% └───┘
+% prints the top of boxes (cels) of a given line
+% N is the number of cells on the line
+print_line_top(0) :- nl.
+print_line_top(N) :-
+  N > 0,
+  put_code(9484), put_code(9472), put_code(9472), put_code(9472), put_code(9488),
+  N1 is N - 1, print_line_top(N1).
+
+% prints the bottom of boxes (cels) of a given line
+% N is the number of cells on the line
+print_line_bot(0) :- nl.
+print_line_bot(N) :-
+  N > 0,
+  put_code(9492), put_code(9472), put_code(9472), put_code(9472), put_code(9496),
+  N1 is N - 1, print_line_bot(N1).
+
+% prints the center of the boxes (cells) and the cell content of a given line
+print_line([]).
+print_line([C | L]) :-
+  print_cell(C),
+  print_line(L).
+
+print_cell(C) :-
+  cell_code(C, S),
+  put_code(9474), write(' '), put_char(S), write(' '), put_code(9474).
+
+% Get 15x15 initial board (checkered)
+initial(-GameState) :-
+  GameState = [
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+  ].
+
+cell_code(0, 'B').
+cell_code(1, 'W').
+
+% calculates the length of a given list and store it on L
+list_length([], 0).
+list_length([_ | X], L) :- list_length(X, N), L is N + 1.
+
+a :-
+write('\33\[31mbold\33\[0m').
