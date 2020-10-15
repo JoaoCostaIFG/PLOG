52faa8cf2609b4888c458ea8509c67b7cc64e307
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-10-15 19:27:52 +0100

    COOLAORS

--- Proj/PLOG_TP1_RI_T3_Emulsion_1.pl
+++ Proj/PLOG_TP1_RI_T3_Emulsion_1.pl
@@ -10,9 +10,9 @@ display_game(+GameState, +Player) :-
 print_board([]).
 print_board([L | B]) :-
   list_length(L, Len),
-  print_line_top(Len),
+  print_line_top(Len), nl,
   print_line(L), nl,
-  print_line_bot(Len),
+  print_line_bot(Len), nl,
   print_board(B).
 
 % ┌───┐
@@ -20,7 +20,7 @@ print_board([L | B]) :-
 % └───┘
 % prints the top of boxes (cels) of a given line
 % N is the number of cells on the line
-print_line_top(0) :- nl.
+print_line_top(0).
 print_line_top(N) :-
   N > 0,
   put_code(9484), put_code(9472), put_code(9472), put_code(9472), put_code(9488),
@@ -28,7 +28,7 @@ print_line_top(N) :-
 
 % prints the bottom of boxes (cels) of a given line
 % N is the number of cells on the line
-print_line_bot(0) :- nl.
+print_line_bot(0).
 print_line_bot(N) :-
   N > 0,
   put_code(9492), put_code(9472), put_code(9472), put_code(9472), put_code(9496),
@@ -42,7 +42,7 @@ print_line([C | L]) :-
 
 print_cell(C) :-
   cell_code(C, S),
-  put_code(9474), write(' '), put_char(S), write(' '), put_code(9474).
+  put_code(9474), write('\33\[1m\33\[34m'), write(S), write('\33\[0m'), put_code(9474).
 
 % Get 15x15 initial board (checkered)
 initial(-GameState) :-
@@ -64,12 +64,10 @@ initial(-GameState) :-
     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
   ].
 
-cell_code(0, 'B').
-cell_code(1, 'W').
+cell_code(0, '\33\[40m B ').
+cell_code(1, '\33\[47m W ').
 
 % calculates the length of a given list and store it on L
 list_length([], 0).
 list_length([_ | X], L) :- list_length(X, N), L is N + 1.
 
-a :-
-write('\33\[31mbold\33\[0m').
