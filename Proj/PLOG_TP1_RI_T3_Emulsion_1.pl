9708d0775288d27de8965438a85e7cbcda4e054c
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-10-17 17:10:26 +0100

    Improved code.

--- Proj/PLOG_TP1_RI_T3_Emulsion_1.pl
+++ Proj/PLOG_TP1_RI_T3_Emulsion_1.pl
@@ -1,50 +1,7 @@
-playIO :-
+play :-
   initial(-GameState),
   Player is 0,
   display_game(+GameState, +Player).
-
-% DRAWING %
-
-% draws a given board/GameState on the console
-display_game(+GameState, +Player) :-
-  print_board(GameState).
-
-print_board([]).
-print_board([L | B]) :-
-  length(L, Len),
-  print_line_top(Len), nl,
-  print_line(L), nl,
-  print_line_bot(Len), nl,
-  print_board(B).
-
-% ┌───┐
-% │ W │
-% └───┘
-% prints the top of boxes (cels) of a given line
-% N is the number of cells on the line
-print_line_top(0).
-print_line_top(N) :-
-  N > 0,
-  put_code(9484), put_code(9472), put_code(9472), put_code(9472), put_code(9488),
-  N1 is N - 1, print_line_top(N1).
-
-% prints the bottom of boxes (cels) of a given line
-% N is the number of cells on the line
-print_line_bot(0).
-print_line_bot(N) :-
-  N > 0,
-  put_code(9492), put_code(9472), put_code(9472), put_code(9472), put_code(9496),
-  N1 is N - 1, print_line_bot(N1).
-
-% prints the center of the boxes (cells) and the cell content of a given line
-print_line([]).
-print_line([C | L]) :-
-  print_cell(C),
-  print_line(L).
-
-print_cell(C) :-
-  cell_code(C, S),
-  put_code(9474), write('\33\[1m\33\[34m'), write(S), write('\33\[0m'), put_code(9474).
 
 % Get 15x15 initial board (checkered)
 initial(-GameState) :-
@@ -66,8 +23,71 @@ initial(-GameState) :-
     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
   ].
 
-cell_code(0, '\33\[40m B ').
-cell_code(1, '\33\[47m W ').
+% DRAWING %
+
+% draws a given board/GameState on the console
+display_game(+GameState, +Player) :-
+  print_board(GameState).
+
+print_board([]).
+print_board([L | B]) :-
+  print_line(L),
+  print_board(B).
+
+% ┌───┐
+% │ W │
+% └───┘
+print_line([]).
+print_line(L) :-
+  print_line_top(L), nl,
+  print_line_center(L), nl,
+  print_line_bot(L), nl.
+
+% prints the top of boxes (cels) of a given line
+print_line_top([]).
+print_line_top([C | L]) :-
+  set_bg_color(+C),
+  set_fg_color(+C),
+  put_code(9484), put_code(9472), put_code(9472), put_code(9472), put_code(9488),
+  reset_ansi,
+  print_line_top(L).
+
+% prints the bottom of boxes (cels) of a given line
+print_line_bot([]).
+print_line_bot([C | L]) :-
+  set_bg_color(+C),
+  set_fg_color(+C),
+  put_code(9492), put_code(9472), put_code(9472), put_code(9472), put_code(9496),
+  reset_ansi,
+  print_line_bot(L).
+
+% prints the center of the boxes (cells) and the cell content of a given line
+print_line_center([]).
+print_line_center([C | L]) :-
+  print_cell(C),
+  print_line_center(L).
+
+print_cell(C) :-
+  cell_code(C, Code),
+  set_bg_color(+C), set_fg_color(+C),
+  put_code(9474), write('\33\[1m'), write(Code), write('\33\[22m'), put_code(9474),
+  reset_ansi.
+
+cell_code(0, ' B ').
+cell_code(1, ' W ').
+cell_fg_color(0, '\33\[34m').
+cell_fg_color(1, '\33\[31m').
+cell_bg_color(0, '\33\[40m').
+cell_bg_color(1, '\33\[47m').
+
+set_fg_color(+C) :-
+  cell_fg_color(C, Color),
+  write(Color).
+set_bg_color(+C) :-
+  cell_bg_color(C, Color),
+  write(Color).
+
+reset_ansi :- write('\33\[0m').
 
 % GAME LOGIC %
 
